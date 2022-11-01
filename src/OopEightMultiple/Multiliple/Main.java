1db1954551dc87c4085dbaaf5565ce6b84597ba0
Maximfsda <lisitsin.maxi@yandex.ru>
2022-11-01 22:39:33 +1000

    Commit OOP(Multiple) -8/3/2

---
+++ src/OopEightMultiple/Multiliple/Main.java
@@ -0,0 +1,8 @@
+package OopEightMultiple.Multiliple;
+
+public class Main {
+    public static void main(String[] args) {
+        MultilipleTable multilipleTable = new MultilipleTable();
+        System.out.println(multilipleTable);
+    }
+}
